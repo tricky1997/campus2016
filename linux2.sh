e9637821372f34a7fe1f1c7a6d6f13c6f2135cc2
JiangHP <599025681@qq.com>
2016-06-16 08:50:31 +0800

    linux exercise 2

---
+++ linux2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#远程文件拷贝,scp命令，具体用法参考网址
+#http://zhidao.baidu.com/link?url=2u4vPS4z2oFb1_EFzDCNfPf4f4fyptLIs48IxDFijUkdmtLZLnAnn3bdQL6h-PwTu2l2Ft9y5BS9xpnAxJAfwK
+scp /dir1/file  root@l-test.dev.cn1:/tmp
