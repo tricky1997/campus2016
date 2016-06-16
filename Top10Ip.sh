a58047e050e3f016343e5a032b6d7580beb4c048
JiangHP <599025681@qq.com>
2016-06-17 00:55:33 +0800

    linux exercise 1: use awk

--- Top10Ip.sh
+++ Top10Ip.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "visit times    IP"  > Top10Ip.txt
-find ~ -name *access_log*.txt | xargs -n 1 cat | cut -d- -f1 | sort -n | uniq -c | sort -nr | head -n 10  >> Top10Ip.txt
+find ~ -name *access_log*.txt 2>/dev/null | xargs -n1 awk '{print $1}' | sort -n|uniq -c|sort -nr |awk 'BEGIN{print "IP\t\tvisit times"} {print $2"\t"$1}'|head -n11
