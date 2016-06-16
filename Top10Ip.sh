f9453dc98f151e0886074fe7f634c426aa3fbd56
XingyuFu <417133251@qq.com>
2016-06-16 10:54:21 +0800

    linux练习题1

--- Top10Ip.sh
+++ Top10Ip.sh
@@ -1,6 +1,6 @@
 # tomcat的access log中格式样例如下：
 # 127.0.0.1 37 [05/Jun/2012:17:23:43 +0800] -- 200 2806 127.0.0.1 8080 GET/cdbleasing/message.listMessagePrompt.action?_dc=1338888223333 HTTP/1.1
-# 拿到第一个字段的ip就足够了
-# 文件名称，如localhost_access_log*.txt文件
+# 拿到第一个字段的ip就足够了。统计ip次数并排序。
+# 文件名称，如localhost_access_log*.txt文件，通配符是 *access_log*.txt
 
 awk -F" " '{print $1}' *access_log*.txt|sort|uniq -c|sort -nr|head -n10
